7ea3d89fc375139e2918550bad91b157d62f8b90
trongngo148 <ngohuutrong.jonathan@gmail.com>
2021-01-27 17:20:07 +0700

    4 files modified

--- src/app/person-add/person-add.component.ts
+++ src/app/person-add/person-add.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-person-add',
-  templateUrl: './person-add.component.html',
-  styleUrls: ['./person-add.component.css']
+  selector: "app-person-add",
+  templateUrl: "./person-add.component.html",
+  styleUrls: ["./person-add.component.css"]
 })
 export class PersonAddComponent implements OnInit {
-
-  constructor() { }
+  formPersonAdd: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.formPersonAdd = this.formBuilder.group({
+      username: [""],
+      password: [""],
+      gender: ["Male"],
+      birthday: [""]
+    });
+    console.log(this.formPersonAdd)
   }
-
 }
